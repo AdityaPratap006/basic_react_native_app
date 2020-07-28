54eab5f2f477363706235cbbd8d48a4868047243
Aditya Pratap <adityapratap7344@gmail.com>
2020-07-28 16:05:20 +0530

    - added image detail component

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const ImageDetail = ({ title, imageSource, imageScore }) => {
+    return (
+        <View style={styles.imageDetail}>
+            <Image source={imageSource} />
+            <Text style={styles.titleText}>{title}</Text>
+            <Text style={styles.scoreText}>Score: {imageScore}</Text>
+        </View>
+    );
+};
+
+export default ImageDetail;
+
+const styles = StyleSheet.create({
+    imageDetail: {
+        width: 250,
+        height: 200,
+        marginVertical: 20,
+        marginHorizontal: 20,
+        borderRadius: 10,
+        backgroundColor: '#2196f3',
+        elevation: 18,
+    },
+    titleText: {
+        fontSize: 18,
+        color: '#fff',
+    },
+    scoreText: {
+        fontSize: 14,
+        color: '#fff',
+    }
+});
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Components
+import ImageDetail from '../components/ImageDetail';
+import { FlatList } from 'react-native-gesture-handler';
+
+const images = [
+    {
+        title: "Forest",
+        src: require("../assets/images/forest.jpg"),
+        score: 6,
+    },
+    {
+        title: "Mountains",
+        src: require("../assets/images/mountain.jpg"),
+        score: 8,
+    },
+    {
+        title: "Beach",
+        src: require("../assets/images/beach.jpg"),
+        score: 9,
+    }
+];
+
 const ImageScreen = () => {
     return (
         <View>
-            <Text>Image Screen</Text>
+            <FlatList
+                data={images}
+                keyExtractor={(img) => img.title}
+                renderItem={({ item }) => (
+                    <ImageDetail
+                        title={item.title}
+                        imageSource={item.src}
+                        imageScore={item.score}
+                    />
+                )}
+            />
         </View>
     );
 };
@@ -12,6 +44,6 @@ const ImageScreen = () => {
 export default ImageScreen;
 
 const styles = StyleSheet.create({
-    
+
 });
 
